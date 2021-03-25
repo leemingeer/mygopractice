cd9a3b77648a3cc5a112733ce38433b950321670
liming1 <liming1@huayun.com>
2021-03-25 20:10:36 +0800

    simple factory

---
+++ designMode/appmain/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"1stgoproj/designMode/services"
+	"fmt"
+)
+
+func main() {
+	//var service1 services.IService = services.NewsService{} // Get() method input para is not address
+	var service1 services.IService = new(services.NewsService) // point method
+	fmt.Println(service1.Get(123))
+	var service2 services.IService = new(services.UserService) // or &services.UserService{}
+	fmt.Println(service2.Get(123))
+
+
+	var service3 services.IService = services.ServiceFactory{}.Create("news")
+	fmt.Println(service3.Get(123))
+
+	var service4 services.IService = new(services.ServiceFactory).Create("news")
+	fmt.Println(service4.Get(123))
+
+
+	var service5 services.IService = services.NewServiceFactory().Create("news")
+	fmt.Println(service5.Get(123))
+
+}
---
+++ designMode/services/NewsService.go
@@ -0,0 +1,9 @@
+package services
+
+type NewsService struct {
+
+}
+
+func (us *NewsService) Get(id int) string{
+	return "single news"
+}
