08ce2b37316e9c1015288b3d949f8a8e127b8a69
leemingeer <leemingeer@gmail.com>
2021-04-06 22:34:04 +0800

    add goroutine test

---
+++ parallel/routeTest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// 构建一个通道
+	ch := make(chan int)
+	// 开启一个并发匿名函数
+	go func() {
+		fmt.Println("start goroutine")
+		// 通过通道通知main的goroutine
+		ch <- 0
+		fmt.Println("exit goroutine")
+	}()
+	time.Sleep(3*time.Second)
+	fmt.Println("wait goroutine")
+	// 等待匿名goroutine
+	<-ch
+	fmt.Println("all done")
+
+
+
+
+
+}
