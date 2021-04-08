f8af9b2e8a5c3621d98ec78dc5363511048ea262
leemingeer <leemingeer@gmail.com>
2021-04-08 23:24:25 +0800

    add slice/map/struct no initial and initial nil

--- slice/basic.go
+++ slice/basic.go
@@ -2,51 +2,18 @@ package main
 
 import "fmt"
 
-var s []int
-// s = append(s, 1)
-// go和c一样，所有的运算都应该在函数内进行，函数外进行的是语法错误。
-// 函数体外初始化结构体就两个办法，要么一次性全部赋值，要么先声明（全局/局部）变量，在某个函数内进行赋值，在函数体外进行结构体成员赋值相当于函数外面进行运算了。
-
-// go不支持在函数外赋值与调用操作, 但可以声明一个变量或者声明并初始化一个变量。
-
 func main(){
-	s = append(s, 1)
-	fmt.Print(s)
-	//[1]
-    f2()
-	f3()
-	f4()
-}
 
+	var s1 []int
+	fmt.Printf("s1: %v, %p\n", s1, &s1) //  [], 0xc00000c060
+	fmt.Println(s1==nil) // true, nil表示没有引用内存
+	fmt.Println(len(s1)==0) //true
 
+	s2 := make([]int, 0)
+	fmt.Printf("s2: %v, %p\n", s2, &s2) // [], 0xc00000c080
+	fmt.Println(s2==nil) // false
+	fmt.Println(len(s2)==0) //true
 
-type person struct {
-	name string
-	age int
-}
-
-// var P person
-//P.name = "annie"
-//P.age = 20
-
-var P1 = person{"annie", 20}
-
-func f2() {
-	fmt.Printf("The person's name is %s\n", P1.name)
-}
-
-
-var P2 person
-
-P2.name = "a"
-P2.age = 10
-fmt.Printf("The person's name is %s\n", P2.name)
-
-}
-
-func f4(){
-	var s []int
-	s = make([]int, 0)
-	s = append(s, 1)
-	fmt.Printf("%v\n", s)
+	s1 = append(s1, 1)
+	fmt.Printf("%v\n", s1) //[1]
 }
---
+++ struct/basic.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Person struct{
+	age int
+	address []string
+}
+
+func main(){
+
+	var one Person
+	fmt.Printf("one: %v,%v, %p\n", one, &one, &one) // {0 []},&{0 [], 0xc0000a6020
+
+	two := Person{}
+	fmt.Printf("two: %v,%v, %p\n", two, &two, &two) //{0 []},&{0 []}, 0xc0000a6080
+
+
+	three := Person{age: 12,
+		address: []string{"shanghai", "SD"},
+	}
+
+	fmt.Printf("three: %v,%v, %p\n", three, &three, &three) //{12 [shanghai SD]},&{12 [shanghai SD]}, 0xc00000c0c0
+
+
+
+}
