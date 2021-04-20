63be66adc3c7e190a28f1b907d75dde3bb4b5411
liming1 <liming1@huayun.com>
2021-04-20 21:00:24 +0800

    add txt and yaml operation

---
+++ fileOperation/readYAMLfile.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+type myData struct {
+	Conf struct {
+		Hits      int64
+		Time      int64
+		CamelCase string `yaml:"camelCase"`
+	}
+}
+
+func readConf(filename string) (*myData, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &myData{}
+	err = yaml.Unmarshal(buf, c)
+	if err != nil {
+		return nil, fmt.Errorf("in file %q: %v", filename, err)
+	}
+
+	return c, nil
+}
+
+func main() {
+	c, err := readConf("conf.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v", c)
+}
