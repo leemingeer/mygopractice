d532522af3f9b88e6a4071fa82f57449dfb1fe3a
leemingeer <leemingeer@gmail.com>
2021-07-14 22:19:32 +0800

    add grpc

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module 1stgoproj
 
 go 1.15
 
-require k8s.io/apimachinery v0.21.0
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	k8s.io/apimachinery v0.21.0
+)
---
+++ map/range.go
@@ -0,0 +1,23 @@
+package main
+import (
+"fmt"
+)
+func main() {
+	s :=[]int{1,2,3,4}
+	m :=make(map[int]*int)
+	for k,v:=range s{
+		n := v
+		fmt.Printf("n addr:%v, value: %d\n", &n, n)
+
+		m[k]=&n
+	}
+	for key, value := range m {
+		fmt.Printf("map[%v]=%v\n", key, *value)
+	}
+	fmt.Println(m)
+
+	for _,  v :=range []int{2,2,2,2,2}{
+		x := v // x addr differs even when v is always 2
+		fmt.Printf("x addr:%v, value: %d\n", &x, x)
+	}
+}
