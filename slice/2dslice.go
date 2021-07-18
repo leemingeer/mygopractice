97ab67bc0eb970ab9302d0a149874a42b0d41452
leemingeer <leemingeer@gmail.com>
2021-07-18 10:43:36 +0800

    add 2d slice

---
+++ slice/2dslice.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	d2slice := make([][]int, 2, 10)
+	fmt.Println(d2slice, len(d2slice), cap(d2slice)) //[[] []] 2 10
+	d2slice = append(d2slice, []int{1,2})
+	d2slice = append(d2slice, []int{3,4})
+	fmt.Println(d2slice, len(d2slice), cap(d2slice)) //[[] [] [1 2] [3 4]] 4 10
+}
---
+++ slice/makeslice.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	slice1 := make([]int, 2, 10)
+	fmt.Println(slice1, cap(slice1), len(slice1))
+
+	slice1 = append(slice1, 1,2)
+	fmt.Println(slice1, len(slice1), cap(slice1))
+
+    slice1 = append(slice1, []int{1,2,3,4,5,6}...)
+	fmt.Println(slice1, len(slice1), cap(slice1))
+
+	slice1 = append(slice1, 7)
+	fmt.Println(slice1, len(slice1), cap(slice1))
+
+
+    slice2 := make([]int, 0 ,10)
+	fmt.Println(slice2, len(slice2), cap(slice2))
+
+
+
+}
