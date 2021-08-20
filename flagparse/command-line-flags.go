d0e3b7af5ea932a090c875d0e7289eb938fae583
liming1 <liming1@huayun.com>
2021-08-20 20:25:51 +0800

    add flag

---
+++ flagparse/command-line-flags.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+"flag"
+"fmt"
+)
+
+func main() {
+    // -word=xxx
+	// second parameter is default
+	// usage show, -h or unsupport --key
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *boolPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args()) // 剩余的参数,[]string
+}
